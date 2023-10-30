a3925224313754c21b238934f2ccad126e033307
Yusdiansyah <147376412+Yusdiansyah@users.noreply.github.com>
2023-10-30 13:00:41 +0700

    Add files via upload

---
+++ Lingkaran.py
@@ -0,0 +1,7 @@
+import math
+
+r = float(input("Masukan jari-jari Lingkaran"))
+luas = math.pi * r**2
+keliling = 2 * math.pi * r
+print(f"Luas dari lingkaran adalah: {luas:.2f}" )
+print(f"Keliling dari lingkaran adalah: {keliling:.2f}" )
